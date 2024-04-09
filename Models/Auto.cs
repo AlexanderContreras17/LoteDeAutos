2cd54554904b868f10ccbe674ffaa7592e7a0f47
AlexanderContreras17 <hatakealexander24@gmail.com>
2024-04-09 12:45:22 -0600

    Agregar archivos de proyecto.

---
+++ Models/Auto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoteDeAutos.Models
+{
+    public class Auto
+    {   
+        public string Marca { get; set; } = null!;
+        public string Modelo { get; set; } = null!;
+        public string Version { get; set; } = null!;
+        public ushort Año { get; set; }
+        public decimal Precio { get; set; }
+        public int Kilometraje { get; set; }
+        public string Motor { get; set; } = null!;
+        public string Transmision { get; set; } = null!;
+        public string Carroceria { get; set; } = null!;
+        public string Descripcion { get; set; } = null!;
+    }
+}
